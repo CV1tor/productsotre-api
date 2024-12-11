7cef5ad4bb32aa787d20736bc5308e705ddc54cc
Carlos <cvitor.dev@gmail.com>
2024-12-11 13:43:41 -0300

    Feature: repositories for new domains

---
+++ src/main/java/br/com/user/productsore/usersApi/repository/CartProductRepository.java
@@ -0,0 +1,12 @@
+package br.com.user.productsore.usersApi.repository;
+
+import br.com.user.productsore.usersApi.domain.cart.Cart;
+import br.com.user.productsore.usersApi.domain.cart.CartProduct;
+import br.com.user.productsore.usersApi.domain.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CartProductRepository extends JpaRepository<CartProduct, UUID> {
+    CartProduct findCartProductByProductAndCart(Product product, Cart cart);
+}
--- src/main/java/br/com/user/productsore/usersApi/repository/CartRepository.java
+++ src/main/java/br/com/user/productsore/usersApi/repository/CartRepository.java
@@ -4,10 +4,14 @@ import br.com.user.productsore.usersApi.domain.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
 
     @Query("SELECT SUM(cp.quantity * p.price) FROM CartProduct cp JOIN Product p ON cp.product.id = p.id WHERE cp.cart.id = :id")
     Integer cartTotalPrice(UUID id);
+
+    Optional<Cart> findByUserId(UUID id);
+
 }
